4582ee56e94c14523ea6ca90fb87f5a4700c5851
wendel245 <48558013+wendel245@users.noreply.github.com>
2021-02-25 23:06:40 -0300

    Create script.js

---
+++ Curso JS/Aula-15/script.js
@@ -0,0 +1,20 @@
+var a = ['carro' ,'moto' , 'caminhao']
+console.log(a[0])
+console.log(a)
+ler = a.length
+a[3] = 'avião' //num.push('avião')
+console.log(a)
+console.log(ler)
+
+let num = [5, 8 ,4]
+console.log(num[1])
+console.log(num)
+l = num.length
+num [3] = 6 //num.push(6)
+console.log(num)
+console.log(l)
+console.log(s)
+
+for(let pos=0; pos < num.length; pos++) {
+    console.log(num[pos])
+}
